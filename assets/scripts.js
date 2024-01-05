fa0f7a81f89aeeecd9e6c1639b5499a63d1054f1
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2024-01-05 09:57:12 -0600

    adding api links to jscript file

---
+++ assets/scripts.js
@@ -0,0 +1,10 @@
+//Youtube api link
+//https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=the%20hunt%20for%20red%20october&type=video&key=YOUR_API_KEY&videoType=movie&channelId=UCx8ultakVd3KEaLdliOcc9Q
+
+//Wikipedia api link - requires some additional reading for all the rest of it
+//https://en.wikipedia.org/w/api.php
+
+//OMDB api link
+//http://www.omdbapi.com/?i=tt3896198&apikey=710f7abf
+
+//Rotten tomatoes needs more research and requires approval from them for a link
