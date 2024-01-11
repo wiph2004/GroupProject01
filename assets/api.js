75f6906802ed889270989e03cce78ac3e36ca2e4
KingCamrin <camrindavis58@gmail.com>
2024-01-10 20:04:29 -0600

    added api js file

--- assets/api.js
+++ assets/api.js
@@ -1,13 +1,11 @@
-'use strict';
+const api_key='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTE4OWExOTBlYWRlZjNmOWZhYTMyZGJlMzRjMzdkNCIsInN1YiI6IjY1OWYzYTFiY2U0ZGRjMDFhNTU2MWZmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Yurp8mk1k1oSDDeI4FrV-ng1FYHIl1r9daJI-vRYWE';
+const imageBaseURL = '"https://image.tmdb.org/t/p/"'
 
-const api_key='http://www.omdbapi.com/?i=tt3896198&apikey=710f7abf';
-const imageBaseURL = ''
-
-const fetchDataFromServer=funcition(url,callback,optionalParam); {
-fetch(url)
-.then(response=>response.json()).
-then(data=>callback(data,optionalParam));
+const fetchDataFromServer=function(url,callback,optionalParam) {
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>callback(data,optionalParam));
 }
 
-
-export {imageBaseURL,api_key,fetchDataFromServer };
+// This is for ES6 modules, which we are not using yet
+// export { imageBaseURL, api_key, fetchDataFromServer };
